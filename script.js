097fff3f032673e9b9325bf6ee56eca419d75425
2020b Bognár Kevin <98013419+b0BogKevin@users.noreply.github.com>
2024-01-26 10:00:14 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 function swapHeadAndTail(p) {
-    let halfWay = Math.floor(p.length/2)
+    let halfWay = Math.ceil(p.length/2)
     let eredmeny = []
-    for (let i = halfWay; i < p.length; i++) {
+    if (p.length%2==0) {
+        for (let i = halfWay; i < p.length; i++) {
         eredmeny.push(p[i])
     }
     for (let j = 0; j < halfWay; j++) {
         eredmeny.push(p[j]);   
     }
     return eredmeny
+    }
+
+    for (let i = halfWay; i < p.length; i++) {
+        eredmeny.push(p[i])
+    }
+    eredmeny.push(p[halfWay-1])
+    for (let j = 0; j < halfWay-1; j++) {
+        eredmeny.push(p[j]);   
+    }
+    return eredmeny
+    
 }
 
 let test1 = [ -1, 2 ];
 let test2 = [ 1, 2, -3, 4, 5, 6, -7, 8 ]
+let test3 = [1,2,3]
 
 console.log(swapHeadAndTail(test1).join())
 console.log(swapHeadAndTail(test2).join())
+console.log(swapHeadAndTail(test3).join())
+
 
